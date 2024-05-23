cc0b0944e706bc75c5138bc483946b8090e931bf
jorlazarte <jorgelazarte81@gmail.com>
2024-05-23 13:36:34 -0300

    ultimos cambios
    modificaciones

--- js/service.js
+++ js/service.js
@@ -1,35 +1,31 @@
 function addProd(iId, iOpc){
 	let iPos = parseInt( document.getElementById('iCntProd').value )
 	let iTot
-	//alert('pp');
-	
-	//alert("id:"+iId);
-	//alert("opcion:"+iOpc);
-	//alert(iPos);
+
 	document.getElementById('msgSinReg').style.display = ( iPos > 0 ? 'none' : 'inline'  )
 
 	if( document.getElementById('tr_'+iId) ){
-		//alert('existe entonces reviso: ' + document.getElementById('td_col4_'+iId).innerHTML);
-		//document.getElementById('td_col4_'+iId).innerHTML = iTot
 
 		if(iOpc == 1){
-			//alert("reviso: td_col4_" + iId + " cantidad ahora: " + document.getElementById('td_col4_'+iId).innerHTML)
 			iTot = parseInt(document.getElementById('td_col4_'+iId).innerHTML) + 1
-			//alert('agrega un producto, tenia ' + document.getElementById('td_col4_'+iId).innerHTML + ' y ahora ' + iTot)
-			//return false;
-			//alert('esto ' + iTot + ' por ' + document.getElementById('td_col5_'+iId).innerHTML )
-			//alert('xxtengo: ' + document.getElementById('td_col4_'+iId).innerHTML + ' y cambia a ' + iTot)
+
 			document.getElementById('td_col4_'+iId).innerHTML = iTot
 		}else{
 			iTot = parseInt(document.getElementById('td_col4_'+iId).innerHTML) - 1
+			
+			if(iTot == 0){
+				delPrd(iId)
+				return false;
+			} 
+				
+
 			document.getElementById('td_col4_'+iId).innerHTML = iTot
 		}
-		//alert(iTot + '*' + document.getElementById('td_col5_'+iId).innerHTML);
-		document.getElementById('td_col6_'+iId).innerHTML = iTot * parseFloat(document.getElementById('td_col5_'+iId).innerHTML)						
+
+		document.getElementById('td_col6_'+iId).innerHTML = (iTot * parseFloat(document.getElementById('td_col5_'+iId).innerHTML) ).toFixed(2)
 
 		return false;
 	}else{
-		//alert('no existe');
 		iPos++
 		document.getElementById('iCntProd').value = iPos
 		document.getElementById('divCntProd').innerHTML = iPos
@@ -45,7 +41,7 @@ function addProd(iId, iOpc){
 
 	/*****************TD 1******************/
 	let objDvTd1 = document.createElement('div')
-	objDvTd1.setAttribute('class', 'divTd')
+	objDvTd1.setAttribute('class', 'divTd col1')
 	objDvTd1.setAttribute('id', 'td_col1_'+iId)
 	objDvTd1.append(iPos)
 	
@@ -55,7 +51,7 @@ function addProd(iId, iOpc){
 	let objDvTd2 = document.createElement('div')
 	let objDvImg = document.createElement('div')
 
-	objDvTd2.setAttribute('class', 'divTd')
+	objDvTd2.setAttribute('class', 'divTd col2')
 	objDvTd2.setAttribute('id', 'td_col2_'+iId)
 	objDvImg.setAttribute('class', 'divImg divImg_'+iId)
 	objDvTd2.append(objDvImg)
@@ -63,14 +59,14 @@ function addProd(iId, iOpc){
 
 	/*****************TD 3******************/
 	let objDvTd3 = document.createElement('div')
-	objDvTd3.setAttribute('class', 'divTd')
+	objDvTd3.setAttribute('class', 'divTd col3')
 	objDvTd3.setAttribute('id', 'td_col3_'+iId)
 	objDvTd3.append( document.getElementById('itemDes_'+iId).innerHTML )
 	objDvTr.append(objDvTd3)
 
 	/*****************TD 4******************/
 	let objDvTd4 = document.createElement('div')
-	objDvTd4.setAttribute('class', 'divTd')
+	objDvTd4.setAttribute('class', 'divTd col4')
 	objDvTd4.setAttribute('id', 'td_col4_'+iId)
 	objDvTd4.append('1')
 	objDvTr.append(objDvTd4)
@@ -78,7 +74,7 @@ function addProd(iId, iOpc){
 
 	/*****************TD 5******************/
 	let objDvTd5 = document.createElement('div')
-	objDvTd5.setAttribute('class', 'divTd')
+	objDvTd5.setAttribute('class', 'divTd  col5')
 	objDvTd5.setAttribute('id', 'td_col5_'+iId)
 	objDvTd5.append( document.getElementById('itemPre_'+iId).innerHTML )
 	
@@ -87,14 +83,14 @@ function addProd(iId, iOpc){
 
 	/*****************TD 6******************/
 	let objDvTd6 = document.createElement('div')
-	objDvTd6.setAttribute('class', 'divTd')
+	objDvTd6.setAttribute('class', 'divTd col6')
 	objDvTd6.setAttribute('id', 'td_col6_'+iId)
 	objDvTd6.append( document.getElementById('itemPre_'+iId).innerHTML )
 	objDvTr.append(objDvTd6)
 
 	/*****************TD 7******************/
 	let objDvTd7 = document.createElement('div')
-	objDvTd7.setAttribute('class', 'divTd')
+	objDvTd7.setAttribute('class', 'divTd col7')
 	objDvTd7.setAttribute('id', 'td_col7_'+iId)
 
 	let objDvImAdd = document.createElement('div')
@@ -115,7 +111,7 @@ function addProd(iId, iOpc){
 	objDvTd8.setAttribute('id', 'td_col8_'+iId)
 
 	let objDvImBack = document.createElement('div')
-	objDvImBack.setAttribute('class', 'imgBack')
+	objDvImBack.setAttribute('class', 'imgBack col8')
 	objDvImBack.setAttribute("onclick", 'backTr('+iId+')' )
 
 	objDvTd8.append(objDvImBack)
